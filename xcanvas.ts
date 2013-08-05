59c7c847b221d9d3cadaa27e0a332df9b3e6f684
Usagi Ito <usagi@WonderRabbitProject.net>
2013-08-05 18:51:55 +0900

    fix bug

--- xcanvas.ts
+++ xcanvas.ts
@@ -707,7 +707,7 @@ module xcanvas {
     
     // get input state in delta frame
     get_state(delta_frame = 0) {
-      if(delta_frame >= input_states_size || delta_frame < 0)
+      if(delta_frame >= this.input_states_size || delta_frame < 0)
         throw 'logic error: out of index';
       
       return this.input_states[delta_frame];
@@ -715,7 +715,7 @@ module xcanvas {
     
     // get delta value from before to current for sticks and triggers in delta frame
     get_delta(value_device: input_e, delta_frame = 0) {
-      if(delta_frame >= input_states_size - 1 || delta_frame < 0)
+      if(delta_frame >= this.input_states_size - 1 || delta_frame < 0)
         throw 'logic error: out of index';
       
       return vector2_t.sub
